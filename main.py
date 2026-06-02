177943404d40972cb4fe652f9af4ca1cf58a10ea
Natasha Libera <tash@artoftash.com>
2026-06-02 12:41:54 +0200

    Created default product list and main store object.

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 """ Best Buy - main.py """
+
+import products
+import store
+
+# setup initial stock of inventory
+product_list = [
+    products.Product("Macbook Air M2", price=1450, quantity=100),
+    products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+    products.Product("Google Pixel 7", price=500, quantity=250),
+]
+
+best_buy = store.Store(product_list)
